22defe9992821de5d4af9bfb50811d2d9d60d616
Thiago Mariotto <thiagomariotto@outlook.com.br>
2022-10-21 00:02:48 -0300

    test: add game tests

---
+++ tests/unit/Game.test.ts
@@ -0,0 +1,21 @@
+import { expect } from 'chai';
+import Game from '../../src/domain/entity/Game';
+
+describe('* Game entity test', function () {
+  it('Should create a new Game', function () {
+    const game = new Game('League Of Legends');
+    game.addCategory(1, true);
+    game.addCategory(2, false);
+    expect(game).to.be.deep.equal({
+      name: 'League Of Legends',
+      categories: [{ id: 1, primary: true }, { id: 2, primary: false }]
+    });
+    expect(game.categories).to.length(2);
+  });
+
+  it('Must not add two primary category a game', function () {
+    const game = new Game('League Of Legends');
+    game.addCategory(1, true);
+    expect(() => game.addCategory(2, true)).to.throw('');
+  });
+});
