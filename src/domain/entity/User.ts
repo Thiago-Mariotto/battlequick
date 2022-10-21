87476e673c69aaf9fe174b94b80d614f1c379483
Thiago Mariotto <thiagomariotto@outlook.com.br>
2022-10-20 23:40:24 -0300

    feat: user class

---
+++ src/domain/entity/User.ts
@@ -0,0 +1,20 @@
+export default class User {
+  private id?: string;
+  private fullname: string;
+  private email: string;
+  private password: string;
+  private nickname: string;
+
+  constructor(fullname: string, email: string, password: string, nickname: string) {
+    if (!this.emailIsValid(email)) throw new Error('Invalid Email');
+    this.fullname = fullname;
+    this.email = email;
+    this.password = password;
+    this.nickname = nickname;
+  }
+
+  private emailIsValid(email: string) {
+    const regex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gi;
+    return regex.test(email);
+  }
+}
