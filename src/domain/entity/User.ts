e90df5d8bc08187a5cf8bc2a654aeda6212d8e76
Thiago Mariotto <thiagomariotto@outlook.com.br>
2022-10-21 00:14:15 -0300

    feat: add role to user class

--- src/domain/entity/User.ts
+++ src/domain/entity/User.ts
@@ -4,6 +4,7 @@ export default class User {
   private email: string;
   private password: string;
   private nickname: string;
+  private role;
 
   constructor(fullname: string, email: string, password: string, nickname: string) {
     if (!this.emailIsValid(email)) throw new Error('Invalid Email');
@@ -11,6 +12,7 @@ export default class User {
     this.email = email;
     this.password = password;
     this.nickname = nickname;
+    this.role = 'member';
   }
 
   private emailIsValid(email: string) {
