dfd44e9eb4d31b779e9a837f094a5e73f07ef29e
Thiago Mariotto <thiagomariotto@outlook.com.br>
2022-10-21 00:03:05 -0300

    feat: add game class

---
+++ src/domain/entity/Game.ts
@@ -0,0 +1,10 @@
+export default class Game {
+  categories: { id: number, primary: boolean }[] = [];
+  constructor(readonly name: string) { }
+
+  public addCategory(id: number, primary: boolean) {
+    const hasPrimaryCategory = this.categories.some((c) => c.primary === true);
+    if (primary === true && hasPrimaryCategory) throw new Error('Primary category already been defined');
+    this.categories.push({ id, primary });
+  }
+}
